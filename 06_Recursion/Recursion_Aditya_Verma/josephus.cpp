3fca1f4f14ed36d257175a51807a2b3edaef6205
Nishit Bansal <nishitbnsl@gmail.com>
2026-04-06 22:27:25 +0530

    Move Linkedlist notes pdf & fix Josephus problem

--- 06_Recursion/Recursion_Aditya_Verma/josephus.cpp
+++ 06_Recursion/Recursion_Aditya_Verma/josephus.cpp
@@ -1,21 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// The real Josephus Problem solution.
-// int main(){
-// 	int n;
-// 	cin >> n;
-// 	if(floor(log2(n)) == ceil(log2(n))){
-// 		cout << 1 << endl;
-// 	}else {
-		
-// 		int msb = log2(n);
-// 		int unset = (n & ~(1<<msb));
-// 		int n = (unset << 1);
-// 		cout << (n | (1<<0));
-// 	}
-// }
-
-int main(){
-	
+// Returns survivor index in 1..n
+int josephus(int n, int k)
+{
+    if (n == 1)
+        return 1;
+    return (josephus(n - 1, k) + k-1) % n + 1;
 }
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+
+    int t, n, k;
+    cin >> t;
+    while(t--){
+        cin >> n >> k; // e.g. 7 3 → 4 (survivor position, 1-indexed)
+        cout << josephus(n, k) << "\n";
+    }
+    return 0;
+}
+
